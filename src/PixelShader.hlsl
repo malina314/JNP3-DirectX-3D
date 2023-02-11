d990ad7aa011efa47d3cc00d38f8fb2a4ea4f792
Mateusz Malinowski <malina314@op.pl>
2023-02-11 20:23:04 +0100

    It compiles with texture * color

--- src/PixelShader.hlsl
+++ src/PixelShader.hlsl
@@ -1,7 +1,7 @@
 struct PSInput
 {
     float4 position : SV_POSITION;
-//     float4 color : COLOR; // probably not needed
+    float4 color : COLOR; // probably not needed
     float2 uv : TEXCOORD;
 };
 
@@ -10,10 +10,10 @@ SamplerState g_sampler : register(s0);
 
 float4 main(PSInput input) : SV_TARGET
 {
-    return g_texture.Sample(g_sampler, input.uv);
+//     return g_texture.Sample(g_sampler, input.uv);
 //     return g_texture.Sample(g_sampler, float2(0.0f, 1.0f));
 //     return max(g_texture.Sample(g_sampler, input.uv), input.color);
-//     return input.color * g_texture.Sample(g_sampler, input.uv);
+    return input.color * g_texture.Sample(g_sampler, input.uv);
 //     return float4(1.0f, 0.0f, 1.0f, 1.0f);
 //     return input.color;
 }
